340ea0122b7146b65011cb116ca58ab44ad67aad
Omar Alazaizeh <omx302@gmail.com>
2021-08-27 20:07:07 +0300

    port update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "use strict";
+require("dotenv").config();
 const { db } = require("./src/models");
 const server = require("./src/server.js");
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 db.sync().then(() => {
   server.start(PORT);
 });
